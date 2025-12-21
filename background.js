09e4dabaa689807700a08681f4ec056952318d07
Asahi Suenaga <alsuenaga25@gmail.com>
2025-12-20 23:47:10 -0500

    Expand new tab detection and update manifest
    Added support for detecting additional new tab URLs (about:newtab, about:home, about:blank) in background.js. Updated manifest.json to include scripts and module type in the background configuration.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onCreated.addListener((tab) => {
   // Check if it's a default blank new tab
-  if (tab.pendingUrl === "chrome://newtab/" || tab.url === "chrome://newtab/") {
+  if (tab.pendingUrl === "chrome://newtab/" || tab.url === "chrome://newtab/" || tab.url === "about:newtab" ||   tab.url === "about:home" || tab.url === "about:blank"  ) {
     chrome.tabs.update(tab.id, {
       url: "https://www.google.com"
     });
