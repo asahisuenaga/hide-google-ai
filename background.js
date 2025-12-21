cbd0fe12c9cb19575dd29ce98a6eb2a19e8b68a4
Asahi Suenaga <alsuenaga25@gmail.com>
2025-12-21 00:06:52 -0500

    Bug Fixes

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
 chrome.tabs.onCreated.addListener((tab) => {
-  // Check if it's a default blank new tab
-  if (tab.pendingUrl === "chrome://newtab/" || tab.url === "chrome://newtab/" || tab.url === "about:newtab" ||   tab.url === "about:home" || tab.url === "about:blank"  ) {
-    chrome.tabs.update(tab.id, {
-      url: "https://www.google.com"
+  const isNewTab = 
+    tab.pendingUrl === "chrome://newtab/" || 
+    tab.url === "chrome://newtab/" ||
+    tab.url === "about:newtab" ||
+    tab.url === "about:home" |
+    tab.url === "about:blank"; 
+
+  if (isNewTab) {
+    chrome.tabs.update(tab.id, { url: "https://www.google.com"
     });
   }
 });
