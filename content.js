c7a57076923a9484495d0ef09dfc11767fa5cf7f
Asahi Suenaga <alsuenaga25@gmail.com>
2025-12-22 23:05:37 -0500

    Squashed commit of the following:
    commit 698f1ef66409596ed16f7940a2e38c6add40bbb0
    Author: Asahi Suenaga <alsuenaga25@gmail.com>
    Date:   Mon Dec 22 23:04:39 2025 -0500
    
        Update content.js
    
    commit ba540f2752da2f0e20e7066f55d635e753d11281
    Author: Asahi Suenaga <alsuenaga25@gmail.com>
    Date:   Mon Dec 22 23:04:32 2025 -0500
    
        updated version number
    
    commit 59143a738519e6f55d75ad692e9f1ff3706cae22
    Author: Asahi Suenaga <alsuenaga25@gmail.com>
    Date:   Mon Dec 22 23:02:03 2025 -0500
    
        updated to javascript

---
+++ content.js
@@ -0,0 +1,46 @@
+const pathname = window.location.pathname;
+const searchParams = new URLSearchParams(window.location.search);
+
+// --- /search logic ---
+if (pathname === "/search") {
+  const observer = new MutationObserver(() => {
+    // Hide AI Mode tab
+    document.querySelectorAll('.olrp5b').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('.Wm5I1e').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('.related-question-pair:has(.XTvndd)').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('[jscontroller=Elkdbc]').forEach(el => el.style.display = 'none');
+    document.querySelectorAll('button.plR5qb').forEach(el => el.style.display = 'none');
+    
+    // Hide AI Overviews
+    const ai_overviews = document.querySelectorAll('.hdzaWe');
+    ai_overviews.forEach(ai_overviews => {
+        ai_overviews.style.display = 'none';
+        ai_overviews.style.visibility = 'hidden';
+        ai_overviews.style.height = '0';
+    });
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+// --- Homepage logic (/ or /?zx=...) ---
+if (
+  pathname === "/" &&
+  (!searchParams.has("q") || searchParams.has("zx"))
+) {
+  const hideHomepageStuff = () => {
+    const btn = document.querySelector('.plR5qb');
+    if (btn) btn.style.display = "none";
+    const containerElement = document.querySelector('.dRYYxd');
+    if (containerElement) {
+      containerElement.style.setProperty('background', 'none');
+    };
+  };
+
+  hideHomepageStuff();
+  const homeObserver = new MutationObserver(hideHomepageStuff);
+  homeObserver.observe(document, { childList: true, subtree: true });
+}
